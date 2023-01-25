7f55a72be355d52a5f996e8dceb9b51655fd2823
kovalen1618 <kovalen1.618@gmail.com>
2023-01-25 13:20:13 -0600

    Updated about + sidebar location functionality

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,21 @@
 import './About.css'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 function About() {
   return (
     <div className='About'>
-      <h1>About</h1>
-
-      <h3>Who?</h3>
-      <ul>
-        <li><span></span> Born and raised in St. Louis, Missouri</li>
-        <li>Grew with 9 siblings in a Russian-Ukranian family</li>
-        <li>Brought up by an architect mother and a biologist father</li>
-      </ul>
-
-      <h3>What?</h3>
-      <ul>
-        <li>Software Developer & Instructor</li>
-        <li>Graduate of LaunchCode Coding Camp in Web Development</li>
-        <li>Classical opera singer & pianist</li>
-      </ul>
-
-      <h3>Why?</h3>
-      <ul>
-        <li>Passionate about bringing real and thoughtful experiences to people</li>
-        <li>Love to teach and love to learn</li>
-        <li>Next to coding are music, books, and mother nature</li>
-      </ul>
+      <div className="about">
+        <h1>About</h1>
+        <hr />
+        <p>Born to Russian parents in 1999 in St. Louis, Missouri.<br /></p>
+        <p>Each day I am inspired by human ingenuity and beauty and want to be a part of it more than anything.<br /></p>
+        {/* Actually add some photos of you doing this shit you dumb fuck */}
+        <p>Most know me as a pianist, bibliophile, and a chef however I find myself making a hobby of everything.</p>
+      </div>
+      
+      
+      <div className='img-container'>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
